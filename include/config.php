b7042f894c2b9d1059a46a0f8240c079ad4c1637
mambax7 <mambax7@gmail.com>
2020-10-25 03:39:05 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ include/config.php
@@ -0,0 +1,46 @@
+<?php
+// -------------------------------------------------------------------------
+//                             Ze Uploader 0.6
+//                                Module for
+//                  XOOPS - PHP Content Management System 1RC3
+//                         < http://xoops.eti.br >
+// -------------------------------------------------------------------------
+//  Based on:
+//  myPHPNUKE Web Portal System - http://myphpnuke.com/
+//  PHP-NUKE Web Portal System - http://phpnuke.org/
+//  Thatware - http://thatware.org/
+// -------------------------------------------------------------------------
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+// -------------------------------------------------------------------------
+//
+//  Author: XoopsOnLine.com
+//  Purpose: Module for upload files with Xoops 1RC3.
+//  Email: xoopsonline@franceonline.fr
+//  Site: http://www.XoopsOnLine.com
+//
+// -------------------------------------------------------------------------
+
+$dir = 'upload_dir/';
+
+$sizemax = '10000000';
+
+$affichage = 'oui';
+
+$liens = 'oui';
+
+$ext[0] = 'gif';
+$ext[1] = 'jpg';
+$ext[2] = 'bmp';
+$ext[3] = 'png';
---
+++ xoops_version.php
@@ -0,0 +1,14 @@
+<?php
+$modversion['name'] = 'Ze Uploader';
+$modversion['version'] = 0.6;
+$modversion['description'] = "Module d'Upload de Fichiers";
+$modversion['credits'] = 'The XOOPS Project';
+$modversion['author'] = 'XoopsOnLine.com';
+$modversion['help'] = '';
+$modversion['license'] = 'GNU/GPL';
+$modversion['official'] = 0;
+$modversion['image'] = 'images/zeuploader_slogo.gif';
+$modversion['dirname'] = 'zeuploader';
+
+// Menu
+$modversion['hasMain'] = 1;
